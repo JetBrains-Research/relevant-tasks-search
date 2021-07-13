9828691acaa0abb9c2b65165172d30a1977552ca
volyomaS <rastoden@gmail.com>
2021-07-13 18:04:46 +0700

    create task method added

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
 from src.models.bert_recommender import BertRecommender
+import random
+
+
+def retrieve(bert_rec: BertRecommender):
+    with open("./data/val_data.tsv", "r", encoding="utf-8") as fin:
+        data = fin.readlines()[1:]
+    random.seed(42)
+    random.shuffle(data)
+    for i in range(10):
+        line = data[i].strip().split("\t")
+        test_task = bert_rec.create_task(
+            step_id=int(line[0]), topic_id=int(line[1]), preprocessed_text=line[2], raw_text=line[3]
+        )
+        tasks = bert_rec.retrieve(test_task)
+        fout = open("./retrieval.txt", "a", encoding="utf-8")
+        fout.write(f"Given task:\n{test_task.raw_text}\nTopic id: {test_task.topic_id}\nStep id: {test_task.step_id}\n")
+        for j, rel_task in enumerate(tasks):
+            fout.write(
+                f"Top {j + 1}:\nTopic id: {rel_task[1].topic_id}\nStep id: {rel_task[1].step_id}\nDistance in graph: {rel_task[2]}\n{rel_task[1].raw_text}\n\n"
+            )
+        fout.close()
+
 
 if __name__ == "__main__":
     bert_rec = BertRecommender(from_file=False)
     bert_rec.train(path_to_train="./data/train_data.tsv")
-    # print(bert_rec.evaluate(path_to_test="./data/val_data.tsv", t=0))
-    tasks = bert_rec.retrieve("statements choose correct statements comments java.", mode="retrieve")
-    for task in tasks:
-        print(task[1].topic_id, task[1].raw_text)
+    print(bert_rec.evaluate(path_to_test="./data/val_data.tsv", t=0))
+    retrieve(bert_rec=bert_rec)
--- src/models/bert_recommender.py
+++ src/models/bert_recommender.py
@@ -57,15 +57,8 @@ class BertRecommender:
 
         for line in tqdm(data, desc="vectors calculating"):
             obj = line.split("\t")
-            vector = self.__calculate_vector(obj[2])
             self.__tasks.append(
-                Task(
-                    step_id=int(obj[0]),
-                    topic_id=int(obj[1]),
-                    preprocessed_text=obj[2],
-                    raw_text=BeautifulSoup(obj[3], "lxml").text,
-                    vector=vector,
-                )
+                self.create_task(step_id=int(obj[0]), topic_id=int(obj[1]), preprocessed_text=obj[2], raw_text=obj[3])
             )
 
         with open("./bert_data.pkl", "wb") as fout:
@@ -86,65 +79,64 @@ class BertRecommender:
         counter = 0
         for line in tqdm(test_data, desc="evaluating"):
             obj = line.split("\t")
-            vector = self.__calculate_vector(obj[2])
-            test_task = Task(
-                step_id=int(obj[0]), topic_id=int(obj[1]), preprocessed_text=obj[2], raw_text=obj[3], vector=vector
+            test_task = self.create_task(
+                step_id=int(obj[0]), topic_id=int(obj[1]), preprocessed_text=obj[2], raw_text=obj[3]
             )
 
             relevant_tasks = self.retrieve(task=test_task, k=k)
 
-            aps[0] += self.__ap_at_k(relevant_tasks=relevant_tasks, test_task=test_task, k=1, t=t)
-            aps[1] += self.__ap_at_k(relevant_tasks=relevant_tasks, test_task=test_task, k=3, t=t)
-            aps[2] += self.__ap_at_k(relevant_tasks=relevant_tasks, test_task=test_task, k=5, t=t)
+            aps[0] += self.__ap_at_k(relevant_tasks=relevant_tasks, k=1, t=t)
+            aps[1] += self.__ap_at_k(relevant_tasks=relevant_tasks, k=3, t=t)
+            aps[2] += self.__ap_at_k(relevant_tasks=relevant_tasks, k=5, t=t)
             dist_sum += self.__graph_distances[relevant_tasks[0][1].topic_id, test_task.topic_id]
             counter += 1
 
         results = list(map(lambda x: x / counter, aps + [dist_sum]))
         return {"map@1": results[0], "map@3": results[1], "map@5": results[2], "mean graph distance": results[3]}
 
-    def retrieve(
-        self, task_text: str = "", task: Task = None, k: int = 5, mode: str = "evaluation"
-    ) -> List[Tuple[float, Task]]:
+    def retrieve(self, task: Task, k: int = 5) -> List[Tuple[float, Task, float]]:
         if len(self.__tasks) == 0:
-            raise Exception("Before evaluation you should use 'train' first")
-        if mode == "evaluation" and task is None:
-            raise Exception("Inappropriate use of 'evaluation' mode. You should pass task")
-        if mode == "retrieve" and task_text == "":
-            raise Exception("Inappropriate use of 'retrieve' mode. You should pass task_text")
-        if mode != "evaluation" and mode != "retrieve":
-            raise Exception(f"Inappropriate mode: {mode}")
-        if mode == "retrieve":
-            vector = self.__calculate_vector(task_text)
-            task = Task(step_id=0, topic_id=0, preprocessed_text=task_text, raw_text=task_text, vector=vector)
+            raise Exception("Before retrieving you should use 'train' first")
 
         relevant_tasks = []
         for train_task in self.__tasks:
             dist = self.__calculate_vector_distance(train_task=train_task, test_task=task, method="cityblock")
             if len(relevant_tasks) < k:
-                relevant_tasks.append((dist, train_task))
+                relevant_tasks.append((dist, train_task, self.__graph_distances[train_task.topic_id, task.topic_id]))
                 relevant_tasks.sort(key=lambda x: x[0])
             elif relevant_tasks[-1][0] > dist:
-                relevant_tasks[-1] = (dist, train_task)
+                relevant_tasks[-1] = (dist, train_task, self.__graph_distances[train_task.topic_id, task.topic_id])
                 relevant_tasks.sort(key=lambda x: x[0])
         return relevant_tasks
 
-    def __calculate_vector(self, text):
+    def create_task(self, step_id: int, topic_id: int, preprocessed_text: str, raw_text: str) -> Task:
+        vector = self.__calculate_vector(preprocessed_text)
+        return Task(
+            step_id=step_id,
+            topic_id=topic_id,
+            preprocessed_text=preprocessed_text,
+            raw_text=BeautifulSoup(raw_text, "lxml").text,
+            vector=vector,
+        )
+
+    def __calculate_vector(self, text) -> np.ndarray:
         input_ids = torch.tensor(self.__tokenizer.encode(text, truncation=True)).unsqueeze(0)
         outputs = self.__model(input_ids)
         last_hidden_states = outputs[0]
+        # return last_hidden_states[0].data[0].numpy().flatten()
         return last_hidden_states[0].data.numpy().mean(axis=0).flatten()
 
-    def __calculate_vector_distance(self, train_task: Task, test_task: Task, method: str = "cityblock"):
+    def __calculate_vector_distance(self, train_task: Task, test_task: Task, method: str = "cityblock") -> float:
         if method not in self.__distance_methods:
             raise Exception(f"Incorrect distance method: {method}")
         return self.__distance_methods[method](train_task.vector, test_task.vector)
 
-    def __ap_at_k(self, relevant_tasks: list, test_task: Task, k: int, t: int) -> float:
+    def __ap_at_k(self, relevant_tasks: list, k: int, t: int) -> float:
         tp = 0
         tp_fp = 0
         precision_sum = 0
         for i in range(k):
             tp_fp += 1
-            tp = tp + 1 if self.__graph_distances[relevant_tasks[i][1].topic_id, test_task.topic_id] <= t else tp
+            tp = tp + 1 if relevant_tasks[i][2] <= t else tp
             precision_sum += tp / tp_fp
         return precision_sum / k
