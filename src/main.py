86bb2b65c7b1e7fd6c15a7d82511934b84bb70fd
volyomaS <rastoden@gmail.com>
2021-07-15 13:20:47 +0700

    relevant task retrieval added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from src.models.bert_recommender import BertRecommender
 import random
+import pickle
 
 
 def retrieve(bert_rec: BertRecommender):
     with open("./data/val_data.tsv", "r", encoding="utf-8") as fin:
         data = fin.readlines()[1:]
+    with open("./data/graph/data.pkl", "rb") as fin:
+        id2name = pickle.load(fin)["id2name"]
     random.seed(42)
     random.shuffle(data)
     for i in range(10):
@@ -13,11 +16,30 @@ def retrieve(bert_rec: BertRecommender):
             step_id=int(line[0]), topic_id=int(line[1]), preprocessed_text=line[2], raw_text=line[3]
         )
         tasks = bert_rec.retrieve(test_task)
-        fout = open("./retrieval.txt", "a", encoding="utf-8")
-        fout.write(f"Given task:\n{test_task.raw_text}\nTopic id: {test_task.topic_id}\nStep id: {test_task.step_id}\n")
+        fout = open("./relevant.tsv", "a", encoding="utf-8")
+        fout.write(
+            "\t".join(
+                [
+                    test_task.raw_text.strip(),
+                    str(test_task.step_id),
+                    str(test_task.topic_id),
+                    id2name[test_task.topic_id],
+                ]
+            )
+            + "\n"
+        )
         for j, rel_task in enumerate(tasks):
             fout.write(
-                f"Top {j + 1}:\nTopic id: {rel_task[1].topic_id}\nStep id: {rel_task[1].step_id}\nDistance in graph: {rel_task[2]}\n{rel_task[1].raw_text}\n\n"
+                "\t".join(
+                    [
+                        rel_task[1].raw_text.strip(),
+                        str(rel_task[1].step_id),
+                        str(rel_task[1].topic_id),
+                        id2name[rel_task[1].topic_id],
+                        str(rel_task[2]),
+                    ]
+                )
+                + "\n"
             )
         fout.close()
 
@@ -25,5 +47,5 @@ def retrieve(bert_rec: BertRecommender):
 if __name__ == "__main__":
     bert_rec = BertRecommender(from_file=False)
     bert_rec.train(path_to_train="./data/train_data.tsv")
-    print(bert_rec.evaluate(path_to_test="./data/val_data.tsv", t=0))
+    print(bert_rec.evaluate(path_to_test="./data/test_data.tsv", t=1))
     retrieve(bert_rec=bert_rec)
