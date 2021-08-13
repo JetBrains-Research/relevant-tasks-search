40d3a5c3f671528cd679c1a022ee63ec5101ed72
volyomaS <rastoden@gmail.com>
2021-08-13 13:11:41 +0700

    sbert finetuning edited, bug fixed and tested

--- src/tools/sbert_finetuning.py
+++ src/tools/sbert_finetuning.py
@@ -1,36 +1,27 @@
+import os
 import pickle
 from torch.utils.data import DataLoader
 from sentence_transformers import SentenceTransformer, InputExample, losses
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
-from tqdm.notebook import tqdm
-import numpy as np
+from tqdm import tqdm
 import pandas as pd
 import random
-from typing import List, Tuple
-import torch
+from typing import List, Dict
+
+from src.tools.rut5_finetuning import read_data, configure_arg_parser
 
 
-def read_data(paths: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    if not isinstance(paths, List):
-        raise Exception(f"You passed {type(paths)} instead of List[str] type")
-
-    data_train = pd.read_csv(paths[0])
-    data_val = pd.read_csv(paths[1])
-    data_test = pd.read_csv(paths[2])
-
-    data_train = data_train.dropna().reset_index(drop=True)
-    data_val = data_val.dropna().reset_index(drop=True)
-    data_test = data_test.dropna().reset_index(drop=True)
-
-    return data_train, data_val, data_test
-
-
-def create_dataset(data_train: pd.DataFrame):
-    # Positive examples
+def positive_samples(data_train: pd.DataFrame, label: float) -> List[InputExample]:
+    """
+    Used for sampling positive examples
+    :param data_train:
+    :param label: label for positive examples
+    :return: list with positive InputExamples
+    """
     lessons_set_train = set(list(data_train.lesson_id))
     k = 7
-    dataset_train = []
-    for lesson_id in lessons_set_train:
+    sampled = []
+    for lesson_id in tqdm(lessons_set_train, desc="positive examples"):
         data_train_sampled = data_train[data_train.lesson_id == lesson_id]
 
         if data_train_sampled.shape[0] < 2:
@@ -47,37 +38,57 @@ def create_dataset(data_train: pd.DataFrame):
                             data_train_sampled.at[index1, "preprocessed_text"],
                             data_train_sampled.at[index2, "preprocessed_text"],
                         ],
-                        label=1.0,
+                        label=label,
                     )
                 )
 
         if len(tmp) >= k:
-            dataset_train.extend(random.sample(tmp, k=k))
+            sampled.extend(random.sample(tmp, k=k))
         else:
-            dataset_train.extend(tmp)
-    positive_size = len(dataset_train)
+            sampled.extend(tmp)
 
-    # Negative examples (randomly)
+    return sampled
+
+
+def random_samples(data_train: pd.DataFrame, label: float, size: int) -> List[InputExample]:
+    """
+    Used for sampling negative examples in random way
+    :param data_train:
+    :param label: label for random examples
+    :param size: size of randomly sampled examples
+    :return: list with random InputExamples
+    """
     data_indexes = list(data_train.index)
-    for i in range(len(dataset_train) * 2):
+    sampled = []
+    for _ in tqdm(range(size), desc="negative random examples"):
         indexes = random.sample(data_indexes, 2)
         a_lesson_id = data_train.at[indexes[0], "lesson_id"]
         b_lesson_id = data_train.at[indexes[1], "lesson_id"]
         if a_lesson_id != b_lesson_id:
-            dataset_train.append(
+            sampled.append(
                 InputExample(
                     texts=[
                         data_train.at[indexes[0], "preprocessed_text"],
                         data_train.at[indexes[1], "preprocessed_text"],
                     ],
-                    label=0.0,
+                    label=label,
                 )
             )
 
-    # Negative examples (from one course, but different sections)
+    return sampled
+
+
+def different_sections_samples(data_train: pd.DataFrame, label: float, positive_size: int) -> List[InputExample]:
+    """
+    Used for sampling negative examples, from one course, different sections
+    :param data_train:
+    :param label:
+    :param positive_size: size of positive part of dataset
+    :return:
+    """
     courses_set_train = set(list(data_train.course_id))
-    all_negative = []
-    for course_id in tqdm(courses_set_train):
+    sampled = []
+    for course_id in tqdm(courses_set_train, desc="negative different sections"):
         data_train_sampled = data_train[data_train.course_id == course_id]
         tmp = []
         for index1 in data_train_sampled.index:
@@ -94,16 +105,25 @@ def create_dataset(data_train: pd.DataFrame):
                             data_train_sampled.at[index1, "preprocessed_text"],
                             data_train_sampled.at[index2, "preprocessed_text"],
                         ],
-                        label=0.5,
+                        label=label,
                     )
                 )
-        all_negative.extend(tmp)
-    dataset_train += random.sample(all_negative, positive_size)
+        sampled.extend(tmp)
 
-    # Negative examples (from one section, but different lessons)
+    return random.sample(sampled, positive_size)
+
+
+def different_lessons_samples(data_train: pd.DataFrame, label: float, positive_size: int) -> List[InputExample]:
+    """
+    Used for sampling negative examples, from one section, different lessons
+    :param data_train:
+    :param label:
+    :param positive_size: size of positive part of dataset
+    :return:
+    """
     sections_set_train = set(list(data_train.section_id))
-    all = []
-    for section_id in tqdm(sections_set_train):
+    sampled = []
+    for section_id in tqdm(sections_set_train, desc="negative different lessons"):
         data_train_sampled = data_train[data_train.section_id == section_id]
         tmp = []
         for index1 in data_train_sampled.index:
@@ -120,22 +140,32 @@ def create_dataset(data_train: pd.DataFrame):
                             data_train_sampled.at[index1, "preprocessed_text"],
                             data_train_sampled.at[index2, "preprocessed_text"],
                         ],
-                        label=0.75,
+                        label=label,
                     )
                 )
-        all.extend(tmp)
-    dataset_train += random.sample(all, positive_size)
+        sampled.extend(tmp)
 
-    # Negative examples (same Stepik's block, different courses)
-    with open("/src/data/output_data/stepik_blocks.pkl", "rb") as fin:
-        stepik_blocks = pickle.load(fin)
+    return random.sample(sampled, positive_size)
 
-    for block in tqdm(stepik_blocks.values()):
+
+def different_courses_samples(
+    data_train: pd.DataFrame, stepik_blocks: Dict[str, List[int]], label: float, positive_size: int
+) -> List[InputExample]:
+    """
+    Used for sampling negative examples, from one Stepik's block, different courses
+    :param data_train:
+    :param stepik_blocks: Stepik's courses blocks
+    :param label:
+    :param positive_size: size of positive part of dataset
+    :return:
+    """
+    sampled = []
+    for block in tqdm(stepik_blocks.values(), desc="negative different courses"):
         data_train_sampled = data_train[data_train.course_id.isin(block)]
         tmp = []
         for index1 in data_train_sampled.index:
             for index2 in data_train_sampled.index:
-                if random.random() > 0.5:
+                if random.random() > 0.25:
                     continue
                 if index2 <= index1:
                     continue
@@ -149,11 +179,48 @@ def create_dataset(data_train: pd.DataFrame):
                             data_train_sampled.at[index1, "preprocessed_text"],
                             data_train_sampled.at[index2, "preprocessed_text"],
                         ],
-                        label=0.25,
+                        label=label,
                     )
                 )
-        all.extend(tmp)
-    dataset_train += random.sample(all, positive_size)
+        sampled.extend(tmp)
+
+    return random.sample(sampled, positive_size)
+
+
+def create_dataset(input_path: str, output_path: str):
+    """
+    Used for dataset creation. Should be called before training
+    :param input_path: path to dir with train/val/test split and stepik_blocks file
+    :param output_path: path to saving datasets
+    :return:
+    """
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"{input_path} doesn't exist")
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    data_train, _, _ = read_data(input_path, load_courses=False)
+
+    dataset_train = []
+
+    # Positive examples
+    dataset_train += positive_samples(data_train, label=1.0)
+    positive_size = len(dataset_train)
+
+    # Negative examples (randomly)
+    multiplier = 2
+    dataset_train += random_samples(data_train, label=0.0, size=len(dataset_train) * multiplier)
+
+    # Negative examples (from one course, but different sections)
+    dataset_train += different_sections_samples(data_train, label=0.5, positive_size=positive_size)
+
+    # Negative examples (from one section, but different lessons)
+    dataset_train += different_lessons_samples(data_train, label=0.75, positive_size=positive_size)
+
+    # Negative examples (same Stepik's block, different courses)
+    with open(input_path + "/stepik_blocks.pkl", "rb") as fin:
+        stepik_blocks = pickle.load(fin)
+    dataset_train += different_courses_samples(data_train, stepik_blocks, label=0.25, positive_size=positive_size)
 
     random.shuffle(dataset_train)
     test_size = 0.0005
@@ -162,26 +229,36 @@ def create_dataset(data_train: pd.DataFrame):
         dataset_train[: int(len(dataset_train) * test_size)],
     )
 
-    # Saving dataset to output_data dir
-    with open("../data/output_data/dataset_train_for_sbert.pkl", "rb") as fout:
+    # Saving dataset to output_path dir
+    with open(output_path + "/dataset_train_for_sbert.pkl", "wb") as fout:
         pickle.dump(dataset_train, fout)
-    with open("../data/output_data/dataset_test_for_sbert.pkl", "rb") as fout:
+    with open(output_path + "/dataset_test_for_sbert.pkl", "wb") as fout:
         pickle.dump(dataset_test, fout)
 
-    return dataset_train, dataset_test
 
+def train(input_path: str, output_path: str, model_name: str = "paraphrase-multilingual-mpnet-base-v2"):
+    """
+    Used for training. Should be called after dataset creation
+    :param input_path: path to dir with datasets files
+    :param output_path: where to save finetuned model
+    :param model_name: pretrained model name
+    :return:
+    """
+    # Loading dataset from input path
+    with open(input_path + "/dataset_train_for_sbert.pkl", "rb") as fin:
+        dataset_train = pickle.load(fin)
+    with open(input_path + "/dataset_test_for_sbert.pkl", "rb") as fin:
+        dataset_test = pickle.load(fin)
 
-def train(dataset_train: List[InputExample], dataset_test: List[InputExample]):
-    model_name = "paraphrase-multilingual-mpnet-base-v2"
     train_batch_size = 16
-    num_epochs = 4
-    model_save_path = "../data/output_data/sbert_finetuned"
+    num_epochs = 1
+    model_save_path = output_path + "/sbert_finetuned"
 
     # Model init and other tools
     model = SentenceTransformer(model_name)
     train_dataloader = DataLoader(dataset_train, shuffle=True, batch_size=train_batch_size)
     train_loss = losses.CosineSimilarityLoss(model=model)
-    evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dataset_test, name="test split")
+    evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dataset_test, name="test-split")
 
     # Training
     model.fit(
@@ -190,18 +267,18 @@ def train(dataset_train: List[InputExample], dataset_test: List[InputExample]):
         epochs=num_epochs,
         evaluation_steps=100,
         output_path=model_save_path,
-        checkpoint_path="../data/output_data/checkpoints",
-        checkpoint_save_steps=2000,
+        checkpoint_path=output_path + "/checkpoints",
+        checkpoint_save_steps=500,
         checkpoint_save_total_limit=1,
     )
 
-
-def main():
-    paths = ["", "", ""]
-    data_train, data_val, data_test = read_data(paths=paths)
-    dataset_train, dataset_test = create_dataset(data_train)
-    train(dataset_train, dataset_test)
+    model.save(model_save_path)
 
 
 if __name__ == "__main__":
-    main()
+    __arg_parser = configure_arg_parser()
+    __args = __arg_parser.parse_args()
+    if __args.task == "create_dataset":
+        create_dataset(__args.input, __args.output)
+    elif __args.task == "train":
+        train(__args.input, __args.output)
